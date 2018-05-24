a5d37d7fa373f315668ae7e3359e8fccfdd403ff
wq051437 <940569280@qq.com>
2018-05-24 11:11:45 +0800

    提交配置文件

--- src/main/java/com/jk/SanPtopProviderApplication.java
+++ src/main/java/com/jk/SanPtopProviderApplication.java
@@ -1,10 +1,12 @@
 package com.jk;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@MapperScan("com.jk.dao")
 @ImportResource("classpath:spring-dubbo-provider.xml")
 public class SanPtopProviderApplication {
 
